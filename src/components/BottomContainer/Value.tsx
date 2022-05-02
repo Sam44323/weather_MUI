b5929cee2452598f2c8efbfebcf4bb6b05927a47
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-03 01:24:05 +0530

    add: added sub_container setup for components

---
+++ src/components/BottomContainer/Value.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Value: React.FC = () => {
+  return <div>Value</div>;
+};
+
+export default Value;
---
+++ src/components/TopContainer/MainData.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainData: React.FC = () => {
+  return <div>MainData</div>;
+};
+
+export default MainData;
