9078ce61ff746a63e580ef8df4f45de7edd463b7
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-03 01:21:11 +0530

    add: added initial container_setup

---
+++ src/container/BottomContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BottomContainer: React.FC = () => {
+  return <div>BottomContainer</div>;
+};
+
+export default BottomContainer;
---
+++ src/container/TopContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopContainer: React.FC = () => {
+  return <div>TopContainer</div>;
+};
+
+export default TopContainer;
