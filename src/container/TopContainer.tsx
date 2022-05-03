d49815dce563725f9a23084f36893d78af030689
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-03 14:17:19 +0530

    add: added initial styles for the topContainer

--- src/container/TopContainer.tsx
+++ src/container/TopContainer.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import { Compress, Thermostat } from "@mui/icons-material";
 
 const TopContainer: React.FC = () => {
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "column",
+        color: "#000",
+        alignItems: "center",
+        paddingLeft: "1rem",
       }}
     >
       <Box
@@ -17,10 +21,16 @@ const TopContainer: React.FC = () => {
         }}
       >
         <Box className="locationContainer">
-          <Typography variant="h4" sx={{ textAlign: "center" }}>
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", fontWeight: "600" }}
+          >
             Warsaw
           </Typography>
-          <Typography variant="h6" sx={{ textAlign: "center" }}>
+          <Typography
+            variant="h6"
+            sx={{ textAlign: "center", fontWeight: "600" }}
+          >
             Broken Clouds
           </Typography>
         </Box>
@@ -31,9 +41,64 @@ const TopContainer: React.FC = () => {
               textAlign: "center",
               fontSize: "4rem",
               letterSpacing: "1.4px",
+              fontWeight: "bold",
             }}
           >
             5°C
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000",
+          color: "#fff",
+          padding: "0.4rem 1rem",
+          borderRadius: "10px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Compress
+            sx={{
+              color: "white",
+            }}
+          />
+          <Typography
+            variant="h6"
+            sx={{
+              marginLeft: "4px",
+            }}
+          >
+            Pressure: 1000
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Thermostat
+            sx={{
+              color: "white",
+            }}
+          />
+          <Typography
+            variant="h6"
+            sx={{
+              marginLeft: "4px",
+            }}
+          >
+            Humidity: 30°C
           </Typography>
         </Box>
       </Box>
