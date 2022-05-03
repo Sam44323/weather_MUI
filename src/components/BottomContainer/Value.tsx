021e41ee8e8c396a9ff1b53501feb0c3cd0ca8c0
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-03 15:35:19 +0530

    add: added custom component config for value

--- src/components/BottomContainer/Value.tsx
+++ src/components/BottomContainer/Value.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
+import { Compress, Thermostat } from "@mui/icons-material";
 
-const Value: React.FC = () => {
-  return <div>Value</div>;
+interface ValueProps {
+  value: string | number;
+  icon: "temp" | "pressure";
+  text: string;
+}
+
+const Value: React.FC<ValueProps> = (props) => {
+  const getIcons = (value: "temp" | "pressure") => {
+    switch (value) {
+      case "temp":
+        return (
+          <Thermostat
+            sx={{
+              color: "white",
+            }}
+          />
+        );
+      case "pressure":
+        return (
+          <Compress
+            sx={{
+              color: "white",
+            }}
+          />
+        );
+      default:
+        return (
+          <Thermostat
+            sx={{
+              color: "white",
+            }}
+          />
+        );
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      {getIcons(props.icon)}
+      <Typography variant="body1" sx={{ textAlign: "center", color: "white" }}>
+        {props.text}: {props.value}
+      </Typography>
+    </Box>
+  );
 };
 
 export default Value;
--- src/container/BottomContainer.tsx
+++ src/container/BottomContainer.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
+
+// components
+import Value from "../components/BottomContainer/Value";
 
 const BottomContainer: React.FC = () => {
-  return <div>BottomContainer</div>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#000",
+        opacity: 0.8,
+        width: "80%",
+        margin: "0 auto",
+        marginTop: "2.4rem",
+        borderRadius: "4px",
+      }}
+    >
+      <Value value={1000} text="Pressure" icon="pressure" />
+    </Box>
+  );
 };
 
 export default BottomContainer;
