b9eb1828647cf4247fa75abcb365d55b2d44c425
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-03 13:49:03 +0530

    add: added initial design setup for frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import "./App.css";
 
+// data
+import Data from "./data/weather.data.json";
+
+// utils
+import { Cloud, Rain, Snow, Sunny } from "./assets/index";
+
 // containers
 import TopContainer from "./container/TopContainer";
 import BottomContainer from "./container/BottomContainer";
 
 const App: React.FC = () => {
+  const [currData, setCurrData] = React.useState<any>(Data[0]);
+
+  const getBgImage = (value: string = "cloud") => {
+    switch (value) {
+      case "cloud":
+        return Cloud;
+      case "rain":
+        return Rain;
+      case "snow":
+        return Snow;
+      case "sunny":
+        return Sunny;
+      default:
+        return Cloud;
+    }
+  };
+
   return (
     <div className="App">
-      <TopContainer />
-      <BottomContainer />
+      <div
+        className="ImageContainer"
+        style={{
+          backgroundImage: `url(${getBgImage(currData.weather[0].main)})`,
+        }}
+      >
+        <TopContainer />
+        <BottomContainer />
+      </div>
     </div>
   );
 };
--- src/container/TopContainer.tsx
+++ src/container/TopContainer.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const TopContainer: React.FC = () => {
-  return <div>TopContainer</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: "1rem",
+      }}
+    >
+      <Box className="locationContainer">
+        <Typography variant="h4" sx={{ textAlign: "center" }}>
+          Warsaw
+        </Typography>
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Broken Clouds
+        </Typography>
+      </Box>
+      <Box className="tempContainer">
+        <Typography
+          variant="h1"
+          sx={{
+            textAlign: "center",
+            fontSize: "4rem",
+            letterSpacing: "1.4px",
+          }}
+        >
+          -5°C
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default TopContainer;
